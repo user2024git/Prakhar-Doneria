4580ab32beb70795e1fd27650459298dbb17006c
user2024git <159011992+user2024git@users.noreply.github.com>
2024-02-05 18:42:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const { Telegraf } = require('telegraf');
 const axios = require('axios');
+const { mdEscape } = require("markdown-escape");
+const fs = require("fs");
+
 
 const app = express();
 
@@ -18,6 +21,105 @@ const botToken = process.env.TELEGRAM_BOT_KEY;
 const bot = new Telegraf(botToken);
 
 const messageProcessingDelay = 1000;
+
+bot.command("yt", async (ctx) => {
+  try {
+    const apiKey = "AIzaSyARIp8blKZ4aecnymh4XsC_dmbSw5S-1_I";
+    const query = ctx.message.text.split(" ").slice(1).join(" ");
+
+    if (!query) {
+      ctx.reply("Please provide a search query for YouTube.");
+      return;
+    }
+
+    const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${encodeURIComponent(
+      query,
+    )}&type=video&key=${apiKey}`;
+    const response = await axios.get(url);
+
+    const video = response.data.items[0];
+
+    if (video) {
+      const videoTitle = video.snippet.title;
+      const videoURL = `https://www.youtube.com/watch?v=${video.id.videoId}`;
+
+      ctx.reply(`Top Result:\nTitle: ${videoTitle}\nURL: ${videoURL}`);
+    } else {
+      ctx.reply("No results found for the given query.");
+    }
+  } catch (error) {
+    console.error(
+      "Error processing YouTube command:",
+      error.response?.data || error.message,
+    );
+    ctx.reply("Error processing the YouTube command. Please try again later.");
+  }
+});
+
+const githubToken = process.env.GITHUB_ACCESS_TOKEN;
+
+bot.command("github", async (ctx) => {
+  try {
+    const commandArguments = ctx.message.text.split(" ").slice(1);
+
+    if (commandArguments.length < 2) {
+      ctx.reply(
+        "Please provide both a search query and a programming language.",
+      );
+      return;
+    }
+
+    const query = commandArguments.slice(0, -1).join(" ");
+    const language = commandArguments[commandArguments.length - 1];
+
+    const githubResponse = await searchGitHub(query, language);
+
+    if (githubResponse.items.length > 0) {
+      const randomIndex = Math.floor(
+        Math.random() * githubResponse.items.length,
+      );
+      const randomRepo = githubResponse.items[randomIndex];
+
+      const repoName = randomRepo.full_name;
+      const resultMessage = `Repository: ${repoName}\nDescription: ${randomRepo.description}\nLink: ${randomRepo.html_url}\n\n`;
+      ctx.reply(resultMessage);
+    } else {
+      ctx.reply(
+        `No GitHub repositories found for the query "${query}" with programming language "${language}".`,
+      );
+    }
+  } catch (error) {
+    console.error("Error processing github command:", error);
+    ctx.reply("Error processing the github command. Please try again later.");
+  }
+});
+
+async function searchGitHub(query, language) {
+  const apiUrl = "https://api.github.com/search/repositories";
+
+  try {
+    const response = await axios.get(
+      `${apiUrl}?q=${query}+language:${language}&sort=stars&order=desc`,
+      {
+        headers: {
+          Authorization: `Bearer ${githubToken}`,
+        },
+      },
+    );
+
+    if (!response.data) {
+      throw new Error(
+        `GitHub API request failed with status ${response.status}`,
+      );
+    }
+
+    return response.data;
+  } catch (error) {
+    console.error("Error in GitHub API request:", error);
+    throw new Error("Failed to retrieve data from GitHub API.");
+  }
+}
+
 
 bot.on('text', async (ctx) => {
   console.log('Received text:', ctx.message.text);
@@ -59,11 +161,7 @@ bot.on('text', async (ctx) => {
   }
 });
 
-try {
-  bot.launch();
-} catch (error) {
-  console.error('Error launching the bot:', error);
-}
+bot.launch();
 
 process.on('unhandledRejection', (error) => {
   console.error('Unhandled Rejection:', error);
