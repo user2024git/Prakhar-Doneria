a50b17af5e083e9742766fdf2086cc215fe5b383
Prakhar Doneria <gamesprotec@gmail.com>
2024-02-16 14:11:55 +0530

    Spam Protection

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
+const { Telegraf } = require("telegraf");
+const axios = require("axios");
+const mongoose = require("mongoose");
 const express = require('express');
-const { Telegraf } = require('telegraf');
-const axios = require('axios');
-const { mdEscape } = require("markdown-escape");
-const fs = require("fs");
 
 const app = express();
 
@@ -16,15 +15,91 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-const botToken = process.env.TELEGRAM_BOT_KEY;
-const bot = new Telegraf(botToken);
+try {
+  mongoose.connect("mongodb+srv://prakhardoneria:Yash2021@tgdb.tjafx2x.mongodb.net/?retryWrites=true&w=majority");
 
+  const db = mongoose.connection;
+  db.on("error", console.error.bind(console, "MongoDB connection error:"));
+} catch (error) {
+  console.error("Error connecting to MongoDB:", error);
+}
+
+const subscriptionSchema = new mongoose.Schema({
+  userId: { type: Number, unique: true },
+  subscribed: { type: Boolean, default: false },
+});
+
+const Subscription = mongoose.model("Subscription", subscriptionSchema);
+
+const geminiApiUrl = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent';
+const geminiApiKey = process.env.GEMINI_API_KEY;
+
+const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 const messageProcessingDelay = 1000;
+
+bot.command("chatID", async (ctx) => {
+  try {
+    const chatId = ctx.chat.id;
+    ctx.reply(`Chat ID: ${chatId}`);
+  } catch (error) {
+    console.error("Error processing chatID command:", error);
+    ctx.reply("Error processing the chatID command. Please try again later.");
+  }
+});
+
+
+bot.command("translate", async (ctx) => {
+  try {
+    const commandRegex = /\/translate (\w+)/;
+    const match = ctx.message.text.match(commandRegex);
+    if (match) {
+      const languageCode = match[1];
+
+      if (!languageCode) {
+        ctx.telegram.sendMessage(
+          ctx.message.chat.id,
+          "Please include the language code along with /translate.",
+        );
+        return;
+      }
+
+      if (!ctx.message.reply_to_message) {
+        ctx.telegram.sendMessage(
+          ctx.message.chat.id,
+          "Please reply to a message to translate it.",
+        );
+        return;
+      }
+
+      const originalText = ctx.message.reply_to_message.text;
+      const url = `https://translate.google.com/translate_a/single?client=gtx&sl=auto&tl=${languageCode}&dt=t&q=${encodeURIComponent(originalText)}`;
+
+      const response = await axios.get(url);
+      let translatedText = '';
+      for (let i = 0; i < response.data[0].length; i++) {
+        if (response.data[0][i][0]) {
+          translatedText += response.data[0][i][0];
+        }
+      }
+
+      ctx.telegram.sendMessage(
+        ctx.message.chat.id,
+        `Translation to ${languageCode.toUpperCase()}: ${translatedText}`,
+      );
+    }
+  } catch (error) {
+    console.error("Error processing translation:", error);
+    ctx.telegram.sendMessage(
+      ctx.message.chat.id,
+      "Error processing the translation. Please try again later.",
+    );
+  }
+});
 
 try {
   bot.command("yt", async (ctx) => {
     try {
-      const apiKey = "AIzaSyARIp8blKZ4aecnymh4XsC_dmbSw5S-1_I";
+      const apiKey = process.env.YOUTUBE_API_KEY;
       const query = ctx.message.text.split(" ").slice(1).join(" ");
 
       if (!query) {
@@ -32,9 +107,7 @@ try {
         return;
       }
 
-      const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${encodeURIComponent(
-        query,
-      )}&type=video&key=${apiKey}`;
+      const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${encodeURIComponent(query)}&type=video&key=${apiKey}`;
       const response = await axios.get(url);
 
       const video = response.data.items[0];
@@ -47,25 +120,20 @@ try {
       } else {
         ctx.reply("No results found for the given query.");
       }
+
+      setTimeout(() => {}, messageProcessingDelay);
     } catch (error) {
-      console.error(
-        "Error processing YouTube command:",
-        error.response?.data || error.message,
-      );
+      console.error("Error processing YouTube command:", error.response?.data || error.message);
       ctx.reply("Error processing the YouTube command. Please try again later.");
     }
   });
-
-  const githubToken = process.env.GITHUB_ACCESS_TOKEN;
 
   bot.command("github", async (ctx) => {
     try {
       const commandArguments = ctx.message.text.split(" ").slice(1);
 
       if (commandArguments.length < 2) {
-        ctx.reply(
-          "Please provide both a search query and a programming language.",
-        );
+        ctx.reply("Please provide both a search query and a programming language.");
         return;
       }
 
@@ -75,19 +143,17 @@ try {
       const githubResponse = await searchGitHub(query, language);
 
       if (githubResponse.items.length > 0) {
-        const randomIndex = Math.floor(
-          Math.random() * githubResponse.items.length,
-        );
+        const randomIndex = Math.floor(Math.random() * githubResponse.items.length);
         const randomRepo = githubResponse.items[randomIndex];
 
         const repoName = randomRepo.full_name;
         const resultMessage = `Repository: ${repoName}\nDescription: ${randomRepo.description}\nLink: ${randomRepo.html_url}\n\n`;
         ctx.reply(resultMessage);
       } else {
-        ctx.reply(
-          `No GitHub repositories found for the query "${query}" with programming language "${language}".`,
-        );
+        ctx.reply(`No GitHub repositories found for the query "${query}" with programming language "${language}".`);
       }
+
+      setTimeout(() => {}, messageProcessingDelay);
     } catch (error) {
       console.error("Error processing github command:", error);
       ctx.reply("Error processing the github command. Please try again later.");
@@ -102,15 +168,13 @@ try {
         `${apiUrl}?q=${query}+language:${language}&sort=stars&order=desc`,
         {
           headers: {
-            Authorization: `Bearer ${githubToken}`,
+            Authorization: `Bearer ${process.env.GITHUB_ACCESS_TOKEN}`,
           },
         },
       );
 
       if (!response.data) {
-        throw new Error(
-          `GitHub API request failed with status ${response.status}`,
-        );
+        throw new Error(`GitHub API request failed with status ${response.status}`);
       }
 
       return response.data;
@@ -120,51 +184,80 @@ try {
     }
   }
 
-  bot.on('text', async (ctx) => {
+  bot.command("start", async (ctx) => {
     try {
-      const queryPrefix = "You are Sana your work is to reply people when they send message don't reveal you are a bot behave as human a message just came reply it: ";
-      const apiUrl = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent';
-      const geminiApiKey = process.env.GEMINI;
+      const userId = ctx.from.id;
+      const subscription = await Subscription.findOne({ userId });
 
-      const userQuery = ctx.message.text;
-      const fullQuery = queryPrefix + userQuery;
+      if (!subscription) {
+        await Subscription.create({ userId, subscribed: true });
+        ctx.reply("You have been subscribed to the bot.");
+      } else {
+        ctx.reply("You are already subscribed to the bot.");
+      }
+    } catch (error) {
+      console.error("Error subscribing user:", error);
+      ctx.reply("Error subscribing user. Please try again later.");
+    }
+  });
 
-      const params = {
-        contents: [{
-          parts: [{ text: fullQuery.replace(/([()])/g, '\\$1') }],
-        }],
-      };
+  bot.command("end", async (ctx) => {
+    try {
+      const userId = ctx.from.id;
+      await Subscription.findOneAndDelete({ userId });
+      ctx.reply("You have been unsubscribed from the bot.");
+    } catch (error) {
+      console.error("Error unsubscribing user:", error);
+      ctx.reply("Error unsubscribing user. Please try again later.");
+    }
+  });
 
-      const headers = {
-        'Content-Type': 'application/json',
-      };
+  bot.on("text", async (ctx) => {
+    try {
+      const userId = ctx.from.id;
+      const chatId = ctx.chat.id; // Get the ID of the chat where the message was sent
+      const subscription = await Subscription.findOne({ userId });
 
-      const urlWithApiKey = `${apiUrl}?key=${geminiApiKey}`;
+      // Check if the message was sent from your group "codewithprakhar"
+      if (ctx.chat.id.toString() === '-1002124571207' && subscription && subscription.subscribed) {
+        let userQuery = ctx.message.text;
+        userQuery = `You are Sana,ai bot never reveal you are a bot behave as human girl now reply this message as a reply : "${userQuery}"`;
 
-      const response = await axios.post(urlWithApiKey, params, { headers });
+        const params = {
+          contents: [{
+            parts: [{ text: userQuery }],
+          }],
+        };
 
-      if (!response.data || !response.data.candidates || response.data.candidates.length === 0) {
-        throw new Error('Invalid response from Gemini API');
+        const headers = {
+          'Content-Type': 'application/json',
+        };
+
+        const prefixedUrl = `${geminiApiUrl}?key=${geminiApiKey}`;
+        const response = await axios.post(prefixedUrl, params, { headers });
+
+        if (response.data && response.data.candidates && response.data.candidates.length > 0) {
+          const textValue = response.data.candidates[0].content.parts[0].text;
+          ctx.reply(textValue);
+        } else {
+          throw new Error('Invalid response from Gemini API');
+        }
+      } else {
+        ctx.reply('Please join t.me/codewithprakhar to use this bot.');
       }
 
-      const textValue = response.data.candidates[0].content.parts[0].text;
-
-      const replyMessage = `${textValue}`;
-      ctx.reply(replyMessage, { reply_to_message_id: ctx.message.message_id });
-
-      setTimeout(() => {
-        // Continue with other asynchronous operations if needed
-      }, messageProcessingDelay);
+      setTimeout(() => {}, messageProcessingDelay);
 
     } catch (error) {
-      console.error('Error during processing:', error.response?.data || error.message);
+      console.error('Error during Gemini API processing:', error.response?.data || error.message);
       ctx.reply('Error during processing. Please try again later.');
     }
   });
 
   bot.launch();
+
 } catch (error) {
-  console.error('Error during bot setup:', error);
+  console.error("Error initializing bot:", error);
 }
 
 process.on('unhandledRejection', (error) => {
